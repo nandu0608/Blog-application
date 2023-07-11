0edf68b8c50a7549a2f5553a0c7693dfe5dfa4d8
nandu0608 <n.k.mishra890@gmail.com>
2023-07-11 11:18:04 +0530

    first

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+require("dotenv").config(); //dotenv is a tool for storing secure data
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const server = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const blogRouter =require('./routes/blog-routes')
+const router = require('./routes/user-routes')
+
+server.use(cors());
+server.use(bodyParser.json());
+server.use(express.json());
+server.use(morgan("combined"));
+server.use("/api/user", router.router);
+server.use("/api/blog", blogRouter.router);
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/Blog-site");
+  console.log("database connected");
+}
+main().catch(err => console.log(err));
+server.listen(8000, () => {
+  console.log("Server started ");
+});
